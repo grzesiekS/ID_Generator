58ea47ae2d92111e086db090bf8c7511453fa341
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-08-31 18:28:16 +0200

    Add if statment for idLength greater then 0

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const randomID = (idLength) => {
-    let id = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charsAmount = characters.length;
-    for(let i = 0; i < idLength; i++) {
-        id += characters.charAt(Math.floor(Math.random() * charsAmount));
+    if(idLength > 0) {
+        let id = '';
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const charsAmount = characters.length;
+        for(let i = 0; i < idLength; i++) {
+            id += characters.charAt(Math.floor(Math.random() * charsAmount));
+        }
+        return id;
     }
-    return id;
 }
 
 module.exports = randomID;
